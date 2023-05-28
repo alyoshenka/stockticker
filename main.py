5884496022f462027b017d51e1bb5f77b5eea9de
Alexi <alexibmost@gmail.com>
2023-05-28 07:37:43 -0700

    Utilize neopolitan, colored ticker

--- main.py
+++ main.py
@@ -1,75 +1,12 @@
-import BoardFunctions.Display as Display
-import BoardFunctions.Board as Board
-import BoardFunctions.Transformer as Transformer
-
-import MessageFunctions.transformations as transformations
-import MessageFunctions.groups as groups
-
-import DataFunctions.collection as collection
-
 import time
+from neopolitan.demos import display as neop
+from neopolitan.board_functions.colors import GREEN, RED
+from data.collection import get_tickers, get_ticker_data
+from data.transformation import ticker_obj_to_string
 
+all_ticker_data = [get_ticker_data(sym) for sym in get_tickers()]
+msg = []
+for tick in all_ticker_data:
+    msg.append(('  ' + ticker_obj_to_string(tick), GREEN if tick['up?'] else RED))
 
-def main():
-    board = Board.Board(32*8)
-    disp = Display.Display(board, 8, 32, size=25)
-
-    frame = []
-    space = [False]*8
-    """
-    for c in groups.lowercase:
-        frame += transformations.letter_form_to_onoff_form(c)
-        frame.append(space)
-        frame.append(space)
-    """
-    
-    tickers = collection.tickers()
-    # initialize the first 3 tickers
-    #for i in range(len(tickers)):
-    for i in range(3):
-        ticker = collection.get_ticker_data(tickers[i])
-        # translate to frame
-        frame += ticker_data_to_letter_form(ticker)
-        for i in range(4):
-            frame.append(space)
-
-
-    arr = transformations.onoff_form_to_array_form(frame)
-    #color = transformations.tf_array_to_color(arr, color=(54, 208, 255))
-    board.data = arr
-
-    ctr = 0
-
-    start_time = time.time()
-    time_between = 0.2
-
-    while not disp.shouldExit:
-        if time.time() - start_time > time_between:
-            
-            frame = Transformer.scroll(frame, wrap=True)
-            arr = transformations.onoff_form_to_array_form(frame)
-            #color = transformations.tf_array_to_color(arr, color=(54, 208, 255))
-            board.data = arr
-
-                   
-            start_time = time.time()
-        disp.loop()
-
-def ticker_data_to_letter_form(tick):
-    space = [False]*8
-    data = []
-
-    color =  (0,255,0) if tick["up?"] else (255,0,0)
-
-    data += transformations.string_to_letter_form(tick["symbol"], (54, 208, 255))
-    data.append(space)
-    data.append(space)
-    data += transformations.string_to_letter_form("$" + str(tick["dollarDelta"]),color)
-    data.append(space)
-    data.append(space)
-    data += transformations.string_to_letter_form(str(tick["percentDelta"]) + "%", color)
-
-    return data
-    
-
-main()
+neop(msg)
